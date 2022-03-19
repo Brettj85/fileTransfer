2c2d98c1982146a31c9170a6e443744ad74562f6
unknown <brettj85@gmail.com>
2022-03-18 21:09:52 -0500

    file transfer utility

---
+++ ScripthostFileTransferUtility/ScripthostFileTransferUtility/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace ScripthostFileTransferUtility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileToCopy= "";
+            //find file name
+            DirectoryInfo originatingDirectory = new DirectoryInfo("PATH");
+            FileInfo[] originatingDirectoryFiles = originatingDirectory.GetFiles();
+            DateTime todaysDate = DateTime.Now.Date;
+            string formatedDate = todaysDate.ToString("MM/dd/yyyy");
+            foreach (var fileName in originatingDirectoryFiles)
+            {
+                string substringToLocate = formatedDate.Replace("/","");
+                if (fileName.ToString().Contains(substringToLocate))
+                {
+                    fileToCopy = fileName.ToString();
+                }
+            }
+            string currentFileLocation = Path.Combine("PATH", Path.GetFileName(fileToCopy));
+            string destinationDirectory = Path.Combine("PATH", Path.GetFileName(fileToCopy));
+            File.Copy(currentFileLocation, destinationDirectory, true);
+        }
+    }
+}
